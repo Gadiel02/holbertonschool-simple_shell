3df92e06c4fa78ce3c3ca838ae18881fc7ea784c
Gadiel02 <gadielrivera02@gmail.com>
2024-04-26 20:08:14 -0400

    nada

---
+++ sim_shell.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+#include <stdio.h>
+#include <unistd.h> /* for STDIN_FILENO */
+
+/**
+ * error - Print error message for command not found
+ * @args: array of arguments from get_tokens
+ * @str_line: string from stdin
+ */
+void error(char **args, char *str_line)
+{
+	fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+	free(str_line);
+	free_array(args);
+	exit(127);
+}
+
+/**
+ * main - Simple System Shell (sh)
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *str_line = NULL;
+	size_t len = 0;
+	ssize_t read_bytes;
+	int on = 1;
+	char **args;
+
+	while (on != 0)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("($) ");
+		read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
+
+		if (read_bytes == -1) /* check if getline is successful */
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			on = 0;
+		}
+		else
+		{
+			remove_newline(&str_line, &read_bytes);
+			if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
+				on = 0;
+			else
+			{
+				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
+				if ((white_spaces(args, str_line)) == 0) /* if white spaces only */
+					return (main());
+				if (access(args[0], X_OK) == 0) /*!args */
+					my_exe(args, environ);
+				else
+					get_path(args, environ); /*!args */
+				if (access(args[0], X_OK) != 0)
+					error(args, str_line);
+				free_array(args);
+			}
+		}
+	}
+	free(str_line);/* always free the str_line form stdin */
+	return (0);
+}
+
